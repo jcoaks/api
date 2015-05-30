ea589a00789800c13ffeeffcf2698f4e4847350a
Juan Robles <robles.juanc@gmail.com>
2015-05-30 12:55:33 -0430

    Agregando conexion con la base de datos

--- webservice.php
+++ webservice.php
@@ -12,16 +12,32 @@ $app->get('/', function () {
 // POST route
 $app->post('/guardar', function () use ($app) 
 {
+	//decodificando la entrada
 	$request = $app->request();
 	$body = $request->getBody();
 	$input = json_decode($body);
 
+	//Guardando el JSON
 	$fp = fopen('archivo.json', 'w');
 	fwrite($fp, json_encode($input));
 	fclose($fp);
 
-	header("Content-Type: application/json");
-	echo json_encode($input);
+	//Variables de la Base de datos
+	$dbname = 'name';
+	$dbuser = 'user';
+	$dbpass = 'pass';
+	$dbhost = 'localhost';
+	$dbtable = "usuario";
+
+	//Connectando a la base de datos
+	$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("No se puede conectar a '$dbhost'");
+	mysql_select_db($dbname) or die("No se puede abrir la db '$dbname'");
+
+	//Insertando en la base de datos
+	$test_query = "INSERT INTO $dbtable SET nombre='".$input->nombre."'";
+	$resultado = mysql_query($test_query);
+
+	return $resultado;
 });
 
 $app->run();
